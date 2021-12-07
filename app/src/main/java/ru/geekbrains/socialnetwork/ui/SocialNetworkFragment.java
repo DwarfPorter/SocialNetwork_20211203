4de598e03bf7c77e02b2740d14abc3b649ec7fdf
DwarfPorter <mrvlmor@gmail.com>
2021-12-07 21:03:47 +0300

    Анимация

--- app/src/main/java/ru/geekbrains/socialnetwork/ui/SocialNetworkFragment.java
+++ app/src/main/java/ru/geekbrains/socialnetwork/ui/SocialNetworkFragment.java
@@ -13,6 +13,7 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -24,6 +25,7 @@ import ru.geekbrains.socialnetwork.data.CardsSource;
 
 public class SocialNetworkFragment extends Fragment {
 
+    private static final int MY_DEFAULT_DURATION = 1000;
     private CardsSource data;
     private SocialNetworkAdapter adapter;
     private RecyclerView recyclerView;
@@ -51,7 +53,7 @@ public class SocialNetworkFragment extends Fragment {
                         R.drawable.nature1,
                         false));
                 adapter.notifyItemInserted(data.size() - 1);
-                recyclerView.scrollToPosition(data.size() - 1);
+                recyclerView.smoothScrollToPosition(data.size() - 1);
                 return true;
             case R.id.action_clear:
                 data.clearCardData();
@@ -103,6 +105,11 @@ public class SocialNetworkFragment extends Fragment {
         DividerItemDecoration itemDecoration = new DividerItemDecoration(getContext(), LinearLayoutManager.VERTICAL);
         itemDecoration.setDrawable(getResources().getDrawable(R.drawable.separator, null));
         recyclerView.addItemDecoration(itemDecoration);
+
+        DefaultItemAnimator animator = new DefaultItemAnimator();
+        animator.setAddDuration(MY_DEFAULT_DURATION);
+        animator.setRemoveDuration(MY_DEFAULT_DURATION);
+        recyclerView.setItemAnimator(animator);
 
         adapter.setOnItemClickListener(new SocialNetworkAdapter.OnItemClickListener() {
             @Override
